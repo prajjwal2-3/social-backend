e1731291aa2a1308601f918d54797253e79b1968
Prajjwal sharma <121849327+prajjwal2-3@users.noreply.github.com>
2024-03-31 13:56:54 +0530

    Update friendcheck.js

--- middleware/friendcheck.js
+++ middleware/friendcheck.js
@@ -9,9 +9,7 @@ function checkMiddleware(req, res, next) {
         const friend = value.Friends.includes(userid)
         const have2 = value.Sentrequest.includes(userid)
         if(have || friend || have2){
-            res.json({
-                msg: "Already sent"
-            })
+            res.send('Already Done)
            
         }else{
             next();
